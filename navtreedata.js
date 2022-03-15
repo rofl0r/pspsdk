a70628eaa8029856f4916f186b5b501ce89b0565
fjtrujy <fjtrujy@users.noreply.github.com>
2022-03-15 16:11:34 +0000

    deploy: cc887d731f635e06fba85118eb5c7ee87746cbc8

---
+++ navtreedata.js
@@ -0,0 +1,79 @@
+/*
+ @licstart  The following is the entire license notice for the JavaScript code in this file.
+
+ The MIT License (MIT)
+
+ Copyright (C) 1997-2020 by Dimitri van Heesch
+
+ Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+ and associated documentation files (the "Software"), to deal in the Software without restriction,
+ including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+
+ The above copyright notice and this permission notice shall be included in all copies or
+ substantial portions of the Software.
+
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING
+ BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+ @licend  The above is the entire license notice for the JavaScript code in this file
+*/
+var NAVTREE =
+[
+  [ "PSPSDK", "index.html", [
+    [ "Modules", "modules.html", "modules" ],
+    [ "Data Structures", "annotated.html", [
+      [ "Data Structures", "annotated.html", "annotated_dup" ],
+      [ "Data Fields", "functions.html", [
+        [ "All", "functions.html", "functions_dup" ],
+        [ "Functions", "functions_func.html", null ],
+        [ "Variables", "functions_vars.html", "functions_vars" ]
+      ] ]
+    ] ],
+    [ "Files", "files.html", [
+      [ "File List", "files.html", "files_dup" ],
+      [ "Globals", "globals.html", [
+        [ "All", "globals.html", "globals_dup" ],
+        [ "Functions", "globals_func.html", "globals_func" ],
+        [ "Variables", "globals_vars.html", "globals_vars" ],
+        [ "Typedefs", "globals_type.html", null ],
+        [ "Enumerations", "globals_enum.html", null ],
+        [ "Enumerator", "globals_eval.html", "globals_eval" ],
+        [ "Macros", "globals_defs.html", "globals_defs" ]
+      ] ]
+    ] ]
+  ] ]
+];
+
+var NAVTREEINDEX =
+[
+"annotated.html",
+"gdb-kernellib_8c.html#aeb706976fd9f4890ffb7f6f88cd4669b",
+"group__Debug.html#ga2221fb7d929e71f8657547b9a3207f45",
+"group__IoFileMgr__Kernel.html#ga1b74e1096d8cd872c7c70b8e5f910b35",
+"group__ThreadMan.html#ga7cac4dc1d21061523172114743ff41a0",
+"gumInternal_8h.html",
+"pspaudio_8h.html#a3693159588718841341652c20bc5140b",
+"pspfpu_8h.html#a1cfc0b4c0be5a081e58998b0248c66b7",
+"pspgu_8h.html#a9e2e83d7cd76a93869f0be03286f12c0",
+"pspinit_8h_source.html",
+"pspkerror_8h.html#ada38ce7dd0ddff755524e736afd6fcacad7d4663940f0bbc6a672cf83a7cbad68",
+"pspnet__adhocctl_8h.html#a9c359a9ec5001771d13a673ebfd8144e",
+"pspsysevent_8h.html",
+"pspusbcam_8h.html#a3eb8381c787f166ffd2dd9dcd72d595eab25bf03d45753aed5bf90688488c9e4c",
+"psputility__netparam_8h.html#a718796c1d4ad56b39917e0f15a57a89b",
+"sceGuOffset_8c.html",
+"structGuContext.html#a9b09e02149ab11a0f8dd0fbbebe38523",
+"structSceKernelAlarmInfo.html",
+"structSceModule.html#a366858eee90c033e666cd86488702104",
+"structSceUtilitySavedataParam.html#a2dc6d28058642d0d0f57e37ac6e3c41e",
+"struct__scemoduleinfo.html#a63c9bd43d3863cf150f7e4a9634daebf",
+"unionScePspUnion128.html#a151131d525d33d388d1c2ffd98b25104"
+];
+
+var SYNCONMSG = 'click to disable panel synchronisation';
+var SYNCOFFMSG = 'click to enable panel synchronisation';
